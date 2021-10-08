4ce209952510d6864b3660b42a9e4269ae077299
anastasiagh <gheorghita.anastasia@isa.utm.md>
2021-10-08 11:38:45 +0300

    lab3

---
+++ lab3/src/module-info.java
@@ -0,0 +1,10 @@
+module lab1 {
+    requires javafx.fxml;
+    requires  javafx.controls;
+    requires java.desktop;
+    requires java.logging;
+    requires json;
+    requires json.simple;
+
+    opens sample;
+}
